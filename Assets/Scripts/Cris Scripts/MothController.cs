3b706596defc439527b322d094ab785d7b9872c5
Crystal <crystal.agerton2015@gmail.com>
2019-01-26 12:03:24 -0800

    Fixed Moth and added moth stuff

--- Assets/Scripts/Cris Scripts/MothController.cs
+++ Assets/Scripts/Cris Scripts/MothController.cs
@@ -9,6 +9,7 @@ public class MothController : MonoBehaviour
     public float sleepySpeed; //the speed the moth falls asleep
 
     private Animator anim;
+
     private Rigidbody2D rb2d;
     private Vector2 velocity;
     private float currentSpeed;
@@ -28,18 +29,23 @@ public class MothController : MonoBehaviour
 
     void FixedUpdate()
     {
-        anim.SetBool("sleeping", sleepyTime);
+        updateAnimations();
+
         ///Adjusting current speed based on if it's outside a light source ("Glow")
         if (sleepyTime)
         {
             if (currentSpeed > 0.001)
-                currentSpeed -= Time.fixedDeltaTime/sleepySpeed;
+                currentSpeed -= Time.fixedDeltaTime / sleepySpeed;
+            rb2d.gravityScale += Time.fixedDeltaTime / sleepySpeed;
         }
         else
+        {
             currentSpeed = speed;
+            rb2d.gravityScale = 0;
+        }
 
         rb2d.MovePosition(rb2d.position + new Vector2(Input.GetAxis("DPadHorizontal2"), Input.GetAxis("DPadVertical2")) * Time.fixedDeltaTime * currentSpeed);
-        //Debug.Log(transform.localScale.x);
+
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -60,5 +66,16 @@ public class MothController : MonoBehaviour
     {
         if (other.tag.Contains("Glow"))
             sleepyTime = false;
+    }
+
+    void updateAnimations()
+    {
+        anim.SetBool("sleeping", sleepyTime);
+
+        //Flip sprite based on where the player is moving
+        if (Input.GetAxis("DPadHorizontal2") == -1)
+            GetComponent<SpriteRenderer>().flipX = true;
+        else if (Input.GetAxis("DPadHorizontal2") == 1)
+            GetComponent<SpriteRenderer>().flipX = false;
     }
 }
