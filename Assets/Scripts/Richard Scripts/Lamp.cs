f36c8fd9def2cfe2732d93ed4b8bd5e796bcaf78
Rikerd <cursedmarkedsasuke@gmail.com>
2019-01-26 00:44:32 -0800

    Lamp
    Permant light source when interacted with by the light character

---
+++ Assets/Scripts/Richard Scripts/Lamp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : Interactable
+{
+    // TEMPORARY
+    private SpriteRenderer sr;
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void mothTriggerEffect()
+    {
+        return;
+    }
+
+    public override void lightTriggerEffect()
+    {
+        active = true;
+
+        sr.color = Color.red;
+    }
+}
