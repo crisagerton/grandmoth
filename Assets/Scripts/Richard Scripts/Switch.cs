998343c4a215b2a85cae7e3732b44d6aacc10292
Rikerd <cursedmarkedsasuke@gmail.com>
2019-01-26 00:53:50 -0800

    Switch Bulb
    Moth interactable switch to turn on bulb  in another area

---
+++ Assets/Scripts/Richard Scripts/Switch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : Interactable
+{
+    public Bulb bulb;
+
+    // TEMPORARY
+    private SpriteRenderer sr;
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void mothTriggerEffect()
+    {
+        active = true;
+
+        sr.color = Color.red;
+
+        bulb.activateBulb();
+    }
+
+    public override void lightTriggerEffect()
+    {
+        return;
+    }
+}
