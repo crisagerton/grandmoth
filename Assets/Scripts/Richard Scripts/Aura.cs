89e72ce0f165289102380d3a579bd2154ecf084a
Rikerd <cursedmarkedsasuke@gmail.com>
2019-01-26 01:32:28 -0800

    Aura
    Implemented Aura
    - When glow is close to Aura object, it glows
    - When glow is no longer over Aura object, it will stop glowing

---
+++ Assets/Scripts/Richard Scripts/Aura.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aura : MonoBehaviour
+{
+    public GameObject glow;
+
+    public void Awake()
+    {
+        glow.SetActive(false);
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Glow")
+            glow.SetActive(true);
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Glow")
+            glow.SetActive(false);
+    }
+}
