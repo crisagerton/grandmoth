e89e0ac50c9489bd32a8de339b9e814c0f729b8e
Crystal <crystal.agerton2015@gmail.com>
2019-01-25 23:28:51 -0800

    Basic Movement

---
+++ Assets/Scripts/Cris Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb2d;
+    private Vector2 velocity;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        velocity = new Vector2(0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(Input.GetAxis("Horizontal"));
+        rb2d.MovePosition(rb2d.position + 
+            new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.fixedDeltaTime * speed);
+    }
+}
