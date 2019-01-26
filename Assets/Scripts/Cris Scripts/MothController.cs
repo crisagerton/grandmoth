06b787341f3d8a86025749d696cb4f26a5c53e42
Crystal <crystal.agerton2015@gmail.com>
2019-01-26 08:56:09 -0800

    Moth Sleepy

--- Assets/Scripts/Cris Scripts/MothController.cs
+++ Assets/Scripts/Cris Scripts/MothController.cs
@@ -5,30 +5,49 @@ using UnityEngine;
 public class MothController : MonoBehaviour
 {
     public float speed;
+    public float sleepySpeed; //the speed the moth falls asleep
 
-    protected Rigidbody2D rb2d;
-    protected Vector2 velocity;
+    private Rigidbody2D rb2d;
+    private Vector2 velocity;
+    private float currentSpeed;
+    private bool sleepyTime; //true if outside a light source
     //add impulse to increase speed and stuff so they can have different accelerations
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         velocity = new Vector2(0, 0);
+        currentSpeed = speed;
     }
 
     void FixedUpdate()
     {
+        ///Adjusting current speed based on if it's outside a light source ("Glow")
+        if (sleepyTime)
+        {
+            if (currentSpeed > 0.001)
+                currentSpeed -= Time.fixedDeltaTime/sleepySpeed;
+        }
+        else
+            currentSpeed = speed;
+
+        Debug.Log(currentSpeed);
+
         rb2d.MovePosition(rb2d.position +
-            new Vector2(Input.GetAxis("Horizontal1"), Input.GetAxis("Vertical1")) * Time.fixedDeltaTime * speed);
+            new Vector2(Input.GetAxis("Horizontal1"), Input.GetAxis("Vertical1")) * Time.fixedDeltaTime * currentSpeed);
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag.Contains("Light"))
+        if (other.tag.Contains("Glow"))
         {
-            //Add it so that it falls asleep 
-            //half a second of slowdown before falling asleep
-            //add public variable for the amount of time before it falls asleep
+            sleepyTime = true;
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Contains("Glow"))
+            sleepyTime = false;
     }
 }
