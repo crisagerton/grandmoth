7d645741a03a27401beffd9eccae4ae324edf956
Dominic Langmesser <domlangmesser@aol.com>
2019-01-27 11:03:02 -0800

    slowfields mostly

---
+++ Assets/SlowField.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowField : MonoBehaviour
+{
+    public float speed = 1;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Light")
+        {
+            col.GetComponent<PlayerController>().speed = speed;
+        }
+    }
+}
