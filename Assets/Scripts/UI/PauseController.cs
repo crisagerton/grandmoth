c9928016414ebb9bb3e203f0258d821baad72b04
Rikerd <cursedmarkedsasuke@gmail.com>
2019-01-26 17:39:14 -0800

    Pause Menu
    Completed Pause Menu

--- Assets/Scripts/UI/PauseController.cs
+++ Assets/Scripts/UI/PauseController.cs
@@ -1,28 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PauseController : MonoBehaviour {
     public GameObject pauseMenu;
     public List<GameObject> menus;
-
-    public Text pauseTitle;
     
     private bool paused;
-
-    private bool player1Pause;
-    private bool player2Pause;
-
-    private SelectOnInput selectOnInput;
+    
 
     void Start()
     {
-        player1Pause = false;
-        player2Pause = false;
-
-        selectOnInput = GetComponent<SelectOnInput>();
-
         pauseMenu.SetActive(false);
         foreach (GameObject menu in menus)
         {
@@ -33,38 +21,12 @@ public class PauseController : MonoBehaviour {
     }
 
     void Update() {
-	    if (Input.GetButtonDown("Start1"))
+	    if (Input.GetButtonDown("Start"))
         {
             if (!paused)
-            {
                 Pause();
-                player1Pause = true;
-                pauseTitle.text = "P1 Pause";
-                selectOnInput.setPlayer1Pause(player1Pause);
-            }
-            else if (paused && player1Pause)
-            {
+            else
                 Resume();
-                player1Pause = false;
-                selectOnInput.setPlayer1Pause(player1Pause);
-            }
-        }
-
-        if (Input.GetButtonDown("Start2"))
-        {
-            if (!paused)
-            {
-                Pause();
-                player2Pause = true;
-                pauseTitle.text = "P2 Pause";
-                selectOnInput.setPlayer2Pause(player2Pause);
-            }
-            else if (paused && player2Pause)
-            {
-                Resume();
-                player2Pause = false;
-                selectOnInput.setPlayer2Pause(player2Pause);
-            }
         }
     }
 
--- Assets/Scripts/UI/SelectOnInput.cs
+++ Assets/Scripts/UI/SelectOnInput.cs
@@ -12,68 +12,29 @@ public class SelectOnInput : MonoBehaviour {
     private EventSystem eventSystem;
     private bool buttonSelected;
 
-    private bool player1Pause = false;
-    private bool player2Pause = false;
-
     private void Awake()
     {
         eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
-        player1Pause = false;
-        player2Pause = false;
     }
 
     private void Update() {
-		if (player1Pause && !buttonSelected)
+		if (directionCheck() && !buttonSelected)
         {
-            if (player1DirectionCheck())
-            {
-                eventSystem.SetSelectedGameObject(selectedObject);
-                buttonSelected = true;
-            }
-        }
-
-        if (player2Pause && !buttonSelected)
-        {
-            if (player2DirectionCheck())
-            {
-                eventSystem.SetSelectedGameObject(selectedObject);
-                buttonSelected = true;
-            }
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
         }
 	}
 
-    private bool player1DirectionCheck()
+    private bool directionCheck()
     {
         if (horizontalControlled && verticalControlled)
-            return Input.GetAxisRaw("DPadHorizontal1") != 0 || Input.GetAxisRaw("DPadVertical1") != 0;
+            return Input.GetAxisRaw("DPadHorizontal") != 0 || Input.GetAxisRaw("DPadVertical") != 0;
         else if (horizontalControlled)
-            return Input.GetAxisRaw("DPadHorizontal1") != 0;
+            return Input.GetAxisRaw("DPadHorizontal") != 0;
         else if (verticalControlled)
-            return Input.GetAxisRaw("DPadVertical1") != 0;
+            return Input.GetAxisRaw("DPadVertical") != 0;
 
         return false;
-    }
-
-    private bool player2DirectionCheck()
-    {
-        if (horizontalControlled && verticalControlled)
-            return Input.GetAxisRaw("DPadHorizontal2") != 0 || Input.GetAxisRaw("DPadVertical2") != 0;
-        else if (horizontalControlled)
-            return Input.GetAxisRaw("DPadHorizontal2") != 0;
-        else if (verticalControlled)
-            return Input.GetAxisRaw("DPadVertical2") != 0;
-
-        return false;
-    }
-
-    public void setPlayer1Pause(bool paused)
-    {
-        player1Pause = paused;
-    }
-
-    public void setPlayer2Pause(bool paused)
-    {
-        player2Pause = paused;
     }
 
     private void OnDisable()
