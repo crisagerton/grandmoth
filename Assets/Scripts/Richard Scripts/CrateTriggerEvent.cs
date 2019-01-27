19cee59af79aa66ed7d242045f0e491ed769b8e2
Rikerd <cursedmarkedsasuke@gmail.com>
2019-01-26 21:23:26 -0800

    Frikken Semicolon

--- Assets/Scripts/Richard Scripts/CrateTriggerEvent.cs
+++ Assets/Scripts/Richard Scripts/CrateTriggerEvent.cs
@@ -7,6 +7,6 @@ public class CrateTriggerEvent : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Crate")
-            return
+            return;
     }
 }
