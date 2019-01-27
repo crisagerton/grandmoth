bd705b4e510b284839ebfdabd25c726439f3257e
Crystal <crystal.agerton2015@gmail.com>
2019-01-27 12:36:05 -0800

    tutorial boost and cutscene progress

---
+++ Assets/Scripts/Cris Scripts/CutsceneMoth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneMoth : MonoBehaviour
+{
+//    public List<float> timings;
+//    private SpriteRenderer spriteRen;
+//    private float timer;
+//    private int currentTimingIndex;
+
+//    // Start is called before the first frame update
+//    void Awake()
+//    {
+//        spriteRen = GetComponent<SpriteRenderer>();
+//        spriteRen.enabled = false;
+//        currentTimingIndex = 0;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+
+//    void FadeOut()
+//    {
+//        float a = timer / timings[currentTimingIndex];
+//        sceneImage.color = new Color(1, 1, 1, 1 - a);
+//    } 
+
+//    void FadeIn()
+//    {
+//        float a = timer / timings[currentTimingIndex];
+//        transImage.color = new Color(1, 1, 1, a);
+//    }
+
+
+//IEnumerator FadeWait(float timeWait)
+//    {
+//        yield return new WaitForSeconds(timeWait);
+//        fading = true;
+//    }
+}
