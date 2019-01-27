b57eea23384caefa8cdc00ba4293e6828edcd38d
Rikerd <cursedmarkedsasuke@gmail.com>
2019-01-27 02:23:40 -0800

    Multi Trigger Doors
    Takes multiple triggers to activate

---
+++ Assets/Scripts/Richard Scripts/MultiDoorTriggers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiDoorTriggers : Interactable
+{
+    public override void mothTriggerEffect()
+    {
+        currentState = States.Active;
+    }
+
+    public override void lightTriggerEffect()
+    {
+        currentState = States.Active;
+    }
+
+    public override void mothLeaveEffect()
+    {
+        return;
+    }
+
+    public override void lightLeaveEffect()
+    {
+        return;
+    }
+
+    public States getCurrentState()
+    {
+        return currentState;
+    }
+}
---
+++ Assets/Scripts/Richard Scripts/SideMultiTriggerDoors.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideMultiTriggerDoors : MonoBehaviour
+{
+    public List<MultiDoorTriggers> doorTriggers = new List<MultiDoorTriggers>();
+
+    public SideDoorAnimation door1;
+    public SideDoorAnimation door2;
+
+    private bool active = false;
+
+    public void Awake()
+    {
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!active && triggerCheck())
+        {
+            door1.startMovement(Interactable.States.Active);
+            door2.startMovement(Interactable.States.Active);
+            active = true;
+        }
+    }
+
+    public bool triggerCheck()
+    {
+        foreach (MultiDoorTriggers doorTrigger in doorTriggers)
+        {
+            if (doorTrigger.getCurrentState() != Interactable.States.Active)
+                return false;
+        }
+
+        return true;
+    }
+}
